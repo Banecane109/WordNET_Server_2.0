6433cf3dde91d610514e5f3633f3bcf60ea38866
Branko <dinicbranko007@gmail.com>
2024-06-13 12:33:59 +0200

    Added CORS Support

--- Program.cs
+++ Program.cs
@@ -16,8 +16,20 @@ namespace WordNET_Server_2._0
             builder.Services.AddTransient<DBContext>();
             builder.Services.AddTransient<IExecutionService, ExecutionService>();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost" || new Uri(origin).Host == "192.168.1.96")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+                });
+            });
+
             var app = builder.Build();
 
+            app.UseCors();
             app.MapControllers();
             app.Run();
         }
